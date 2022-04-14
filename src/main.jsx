f7d64954eb334051a7192fedac8152e7f3f5e1eb
unknown <crsn1111@gmail.com>
2022-04-14 19:29:09 +0900

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
 import store from "./redux/configureStore";
 import App from "./shared/App";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
